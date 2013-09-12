26dedc05da2b341d79879a950bc33c0cfff28ba4
josafa <josafa@linuxhost>
2013-09-12 11:03:07 -0300

    Criação da classe Usuario.

---
+++ TesteUsuario/src/main/java/teste/usuario/model/Usuario.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste.usuario.model;
+
+/**
+ *
+ * @author josafa
+ */
+public class Usuario {
+    
+}
